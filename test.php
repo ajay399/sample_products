e7ea690d1d153344791c6be3449106bcfe3b5359
Ajay Rabari <51745812+ajay399@users.noreply.github.com>
2023-07-18 14:03:56 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,83 +1,14 @@
-<?php
+(1) Hide + & - Button in the /cart page
+Dawn theme file name : main-cart-items.liquid
+Code : 
 
-/* This file explains how to use metafields
-
-Once the API is successful you get the metafields at here : https://prnt.sc/uobt3QaZiOsp
-
-In liquid files & app blocks you can retrieve the metafields like this : https://prnt.sc/x-oPUt7qhvzM
-
-This is how output will work in the store : https://prnt.sc/qxDyU7IzNy9u    */
-
-function performShopifyRequest($shop, $token, $resource, $params = array(), $method = 'GET') {
-  $url = "https://{$shop}/admin/{$resource}.json";
-
-  $curlOptions = array(
-    CURLOPT_RETURNTRANSFER => TRUE
-  );
-
-  if ($method == 'GET') {
-    if (!is_null($params)) {
-      $url = $url . "?" . http_build_query($params);
-    }
-  } else {
-    $curlOptions[CURLOPT_CUSTOMREQUEST] = $method;
-  }
-
-  $curlOptions[CURLOPT_URL] = $url;
-
-  $requestHeaders = array(
-    "X-Shopify-Access-Token: ${token}",
-    "Accept: application/json"
-  );
-
- if ($method == 'POST' || $method == 'PUT' || $method == 'DELETE') {
-    $requestHeaders[] = "Content-Type: application/json";
-
-    if (!is_null($params)) {
-      $curlOptions[CURLOPT_POSTFIELDS] = json_encode($params);
-    }
-  }
-
-  $curlOptions[CURLOPT_HTTPHEADER] = $requestHeaders;
-
-  $curl = curl_init();
-  curl_setopt_array($curl, $curlOptions);
-  $response = curl_exec($curl);
-  curl_close($curl);
-
-  return json_decode($response, TRUE);
-}
+ {% if item.product.type != "SAMPLE PRODUCT" %}
+                      <quantity-input class="quantity cart-quantity">
+</quantity-input>quantity-input class="quantity cart-quantity">
+{% endif %}
 
 
-$shop = "ajay-testing-demo.myshopify.com";
-$accessToken = "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";
+(2) Product Page & Cart Page Target variables
 
-
-
-$data = [];
-$data['app_enable'] = "1";
-$data['msg'] = "abcd";
-
-
-
-	$params = array('metafield' => 
-        array(
-            'key' => 'Settings',
-            'value' => json_encode($data),
-            'type' => 'json_string',               
-            'namespace' => 'demo_app'
-        )
-   				 ); 
-        $addMeta= performShopifyRequest($shop,$accessToken,"api/2023-04/metafields",$params,"POST"); 
-        print_r($addMeta);
-
-
- /* This file explains how to use metafields
-
-Once the API is successful you get the metafields at here : https://prnt.sc/uobt3QaZiOsp
-
-In liquid files & app blocks you can retrieve the metafields like this : https://prnt.sc/x-oPUt7qhvzM
-
-This is how output will work in the store : https://prnt.sc/qxDyU7IzNy9u    */
-
-	?>
+window.cartPageSelector = document.querySelector(".cart__ctas");
+window.prodPageSelector = document.querySelector(".product-form__buttons"); 
