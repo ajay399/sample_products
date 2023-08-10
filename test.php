5ce3f8cc4526f6adca4b334d6320dacd59155684
Ajay Rabari <51745812+ajay399@users.noreply.github.com>
2023-08-10 14:13:28 +0530

    Update test.php

--- test.php
+++ test.php
@@ -8,7 +8,36 @@ Code :
 {% endif %}
 
 
-(2) Product Page & Cart Page Target variables
+(2) Product Page & Cart Page Target variables  
+// note set both variables
 
 window.cartPageSelector = document.querySelector(".cart__ctas");
 window.prodPageSelector = document.querySelector(".product-form__buttons"); 
+
+
+(3) Currency Variable
+
+window.ShopMoneySymbolVar = '₹';
+
+
+(4) Cart Drawer Trigger
+$(".ProductForm__AddToCart, .cart_dr").click(function()
+               {
+                 Zs_intervalId = setInterval(function() {
+                if ($('#sidebar-cart').attr('aria-hidden') == "false") {
+                  if(allowedSamples > '0')
+                  {
+                    $(".CartDrawer_sampleProdBtnWrapper").html($(".sampleProdBtnWrapper").html());
+                  }
+                  else
+                  {
+                    $(".CartDrawer_sampleProdBtnWrapper").html();
+                  }
+                    clearInterval(Zs_intervalId);
+                }
+               },500);
+                 
+                 setTimeout(function() {
+                clearInterval(Zs_intervalId);
+            }, 10000); 
+               });
